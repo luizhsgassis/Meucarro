1a2b1b8b3610445731ec5a69bfced730c98ac21a
luizhsgassis <luizhsgassis.dev@gmail.com>
2024-10-04 23:28:18 -0300

    barra de navegação feita e classes do css feitas também

--- index.php
+++ index.php
@@ -3,14 +3,35 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css">
     <title>Meucarro</title>
 </head>
 <body>
     <?php 
-    
+
     
     
     
     ?>
+    <section>
+        <div class="container">
+            <nav class="display_flex align_items_center justify_content_space_between">
+                <img src="./images/MeuCarro.svg" alt="">
+                <div class="display_flex align_items_center">
+                    <ul class="display_flex gap">
+                        <li><a href="#"><p>Comprar</p></a></li>
+                        <li><a href="#"><p>Vender</p></a></li>
+                        <li><a href="#"><p>Serviços</p></a></li>
+                        <li><a href="#"><p>Notícias</p></a></li>
+                        <li><a href="#"><p>Ajuda</p></a></li>
+                    </ul>
+                    <div class="display_flex">
+                        <div class="button secondary_button"><a href="#"><p class="text_button">Entrar</p></a></div>
+                        <div class="button primary_button"><a href="#"><p class="text_button">Cadastrar</p></a></div>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </section>
 </body>
 </html>
